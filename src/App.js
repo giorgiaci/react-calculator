faf565f87830550687d73ed5af15449ee227c757
giciulla <giorgia.ciulla@eng.it>
2022-11-14 11:09:36 +0000

    react calculator initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const operators = ['/', '*', '+', '-', '.'];
+
+  const updateCalc = value => {
+    if(
+      (operators.includes(value) && calc === '') ||
+      (operators.includes(value) && operators.includes(calc.slice(-1))) 
+      ){
+        return
+    }
+    setCalc(calc + value);
+
+    if(!operators.includes(value)){
+      setResult(eval(calc+value).toString());
+    }
+  }
+
+  const calculate = () =>{      
+    if(operators.includes(calc.slice(-1))){
+      return;
+    }
+    setCalc(eval(calc).toString()); 
+    
+  }
+
+  const createDigits = () =>{
+    const digits = [];
+    for(let i = 1; i<10; i++){
+      digits.push(<button key={i} onClick={()=> updateCalc(i.toString())}>{i}</button>);
+    }
+    return digits;
+  }
+
+  const AC = () => {
+    setCalc("");
+    setResult("");
+  } 
+
+  const delLastDigits = () => {     
+    
+    const value = calc.slice(0, -1); 
+
+    if(calc === ''){
+      return;
+    }
+    
+    setCalc( operators.includes(calc.slice(-1)) ? value : value.slice(0, -1));      
+    setResult(eval(operators.includes(calc.slice(-1)) ? value : value.slice(0, -1)));        
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap">
+    <div className="ggHeader">
+      <h1>Giorgia's calculator</h1>
     </div>
+    <div className="App">
+
+     
+      <div className="calculator">
+        
+        <div className="display">
+          { result ? <span>({result})</span> : ''}&nbsp; { calc || "0"}
+        </div>
+      
+        <div className="erase">
+          <button onClick={ AC }>AC</button>         
+          <button onClick={ delLastDigits }>DEL</button>
+        </div>
+              
+      <div className="flex-container">
+        <div className="digits">
+          {createDigits( )}
+          <button>0</button>
+          <button>.</button>
+          <button  onClick={ () => calculate() }>=</button>
+        </div>
+        
+        <div className="operators">
+          <button onClick={ () => updateCalc('/')}>/</button>
+          <button onClick={ () => updateCalc('*')}>*</button>
+          <button onClick={ () => updateCalc('+')}>+</button>
+          <button onClick={ () => updateCalc('-')}>-</button>        
+        </div>   
+      </div>
+        
+      </div>
+    </div>
+    </div>
+    
   );
 }
 
